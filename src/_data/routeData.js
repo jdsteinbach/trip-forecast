5df62b8470b9ba543f432806108f22ac6686cd2a
James Steinbach <jdsteinbach@gmail.com>
2023-11-08 10:57:12 -0700

    replace axios with eleventy-fetch

--- src/_data/routeData.js
+++ src/_data/routeData.js
@@ -1,4 +1,4 @@
-const axios = require('axios').default;
+const EleventyFetch = require("@11ty/eleventy-fetch");
 const routes = require('./routes.json');
 
 require('dotenv').config();
@@ -10,21 +10,22 @@ const routeData = routes.map((r) => {
 
   const stopsForecast = Promise.all(
     r.stops.reverse().map(async (s) => {
-      const forecast = await axios
-        .get(
-          `https://api.openweathermap.org/data/3.0/onecall?lat=${s.lat}&lon=${s.lon}&units=imperial&appid=${OPENWEATHER_APIKEY}`
-        )
-        .then(({ data }) => {
-          return {
-            ...s,
-            data,
-          };
-        })
-        .catch((e) => {
-          console.error(e);
-          throw new Error(e);
-        });
-      return forecast;
+      try {
+        const forecast = await EleventyFetch(
+          `https://api.openweathermap.org/data/3.0/onecall?lat=${s.lat}&lon=${s.lon}&units=imperial&appid=${OPENWEATHER_APIKEY}`,
+          {
+            duration: "1d",
+            type: "json"
+          });
+
+        return {
+          ...s,
+          data: forecast,
+        };
+      } catch(e) {
+        console.error(e);
+        return s;
+      }
     })
   );
 
